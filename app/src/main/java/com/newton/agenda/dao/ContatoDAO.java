b654490dd6c5c592f240da591de74f9ff826a67c
newton <newtoljunior@hotmail.com>
2017-03-16 17:31:33 -0300

    subindo outra parte do banco

--- app/src/main/java/com/newton/agenda/dao/ContatoDAO.java
+++ app/src/main/java/com/newton/agenda/dao/ContatoDAO.java
@@ -2,10 +2,15 @@ package com.newton.agenda.dao;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 import com.newton.agenda.model.Contato;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * Created by PAS12 on 15/03/2017.
@@ -29,6 +34,30 @@ public class ContatoDAO {
         long inserir = db.insert(BANCONOME,null,dados);
         db.close();
         Log.i(BANCONOME,inserir + " ");
+    }
+
+    public List<Contato> buscarContato(){
+
+        String sql = "SELECT * FROM " + BANCONOME;
+        dao = new BancoDAO(context);
+        SQLiteDatabase db = dao.getReadableDatabase();
+        Cursor c = db.rawQuery(sql,null);
+        List<Contato> contatos = new ArrayList<Contato>();
+
+        while (c.moveToNext()){
+
+            Contato contato = new Contato();
+            contato.setId(c.getInt(c.getColumnIndex("id")));
+            contato.setNome(c.getString(c.getColumnIndex("nome")));
+            contato.setEmail(c.getString(c.getColumnIndex("email")));
+            contato.setTelefone(c.getString(c.getColumnIndex("telefone")));
+
+            contatos.add(contato);
+        }
+
+        c.close();
+
+        return  contatos;
     }
 
     private ContentValues pegaDadosDoContato(Contato contato){
